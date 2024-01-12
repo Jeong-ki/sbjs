83a80f71bcd3a9ce0f5b7cafcaaaaf383be3a9bc
Jeong-ki <7863z@naver.com>
2024-01-12 14:26:27 +0900

    ep03 web-server

---
+++ 03.web-server/server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("dist"));
+app.use(cors());
+
+app.get("/api/test", (req, res) => {
+  res.json({
+    name: "Jeong",
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
